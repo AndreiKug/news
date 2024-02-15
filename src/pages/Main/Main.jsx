d9860ac2f9db368b7da46f8880870753158898d3
AndreiKug <anarki3@yandex.ru>
2024-02-15 20:49:17 +0300

    add search input and debounce hook

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -6,6 +6,8 @@ import styles from './styles.module.css';
 import Skeleton from '../../components/Skeleton/Skeleton';
 import Pagination from '../../components/Pagination/Pagination';
 import Categories from '../../components/Categories/Categories';
+import Search from '../../components/Search/Search';
+import { useDebounce } from '../../helpers/hooks/useDebounce';
 
 
 
@@ -15,9 +17,12 @@ const Main = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [categories, setCategories] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState('All');
+    const [keywords, setKeywords] = useState('');
 
     const totalPages = 10;
     const pageSize = 10;
+
+    const debouncedKeywords = useDebounce(keywords, 1500);
 
     const fetchNews = async (currentPage) => {
         try {
@@ -25,7 +30,8 @@ const Main = () => {
             const response = await getNews({
                 currentPage: currentPage, 
                 pageSize: pageSize,
-                category: selectedCategory === 'All' ? null : selectedCategory
+                category: selectedCategory === 'All' ? null : selectedCategory,
+                keywords: debouncedKeywords
             });
             setNews(response.news);
             setIsLoading(false);
@@ -49,7 +55,7 @@ const Main = () => {
     
     useEffect(() => {
         fetchNews(currentPage);
-    }, [currentPage, selectedCategory])
+    }, [currentPage, selectedCategory, debouncedKeywords])
 
     const handleNextPage = () => {
         if (currentPage < totalPages) {
@@ -74,6 +80,8 @@ const Main = () => {
                 selectedCategory={selectedCategory} 
                 setSelectedCategory={setSelectedCategory}
             />
+
+            <Search keywords={keywords} setKeywords={setKeywords}/>
 
             {news.length > 0 && !isLoading ? (
                 <NewsBanner item={news[0]}/>
