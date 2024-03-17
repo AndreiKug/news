7c449db3a1b8041ff6d0e4837516e896c7acaed5
AndreiKug <anarki3@yandex.ru>
2024-03-18 01:06:24 +0300

    add context and theme

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,13 +2,12 @@ import LatestNews from '../../components/LatestNews/LatestNews';
 import NewsByFilters from '../../components/NewsByFilters/NewsByFilters';
 import styles from './styles.module.css';
 
-
 const Main = () => {
     return ( 
         <main className={styles.main}>
             <LatestNews />
 
-            <NewsByFilters />
+            <NewsByFilters/>
                        
         </main>
      );
